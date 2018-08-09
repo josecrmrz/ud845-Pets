ac49051c92e7034cb213e3ea796195d6166df7a2
Jose Ramirez <josecrmrz@gmail.com>
2018-08-08 22:55:58 -0500

    Add PetDbHelper

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -11,10 +11,10 @@ public final class PetContract {
         public static final String TABLE_NAME = "pets";
 
         public static final String COLUMN_PET_ID = BaseColumns._ID;
-        public static final String COLUMN_PET_NAME = "subtitle";
-        public static final String COLUMN_PET_BREED = "subtitle";
-        public static final String COLUMN_PET_GENDER = "subtitle";
-        public static final String COLUMN_PET_WEIGHT = "subtitle";
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
